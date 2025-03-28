6d40113438196dac850510e9ca7391e16ec59555
saeedAbdulraheem-ui <84268217+saeedAbdulraheem-ui@users.noreply.github.com>
2025-03-28 17:46:22 +0000

    Update compile.sh
    modified to use python3 rather than python

--- unidepth/ops/knn/compile.sh
+++ unidepth/ops/knn/compile.sh
@@ -2,4 +2,4 @@
 
 export TORCH_CUDA_ARCH_LIST="6.1 7.0 7.5 8.0 8.6 8.7 8.9 9.0+PTX" 
 # export FORCE_CUDA=1 #if you do not actually have cuda, workaround
-python setup.py build install
+python3 setup.py build install
